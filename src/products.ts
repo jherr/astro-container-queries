d508457773ba0e8615c5b10b61c2f19c936dd848
Jack Herrington <jherr@pobox.com>
2023-09-10 09:38:02 -0700

    First commit

---
+++ src/products.ts
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: "dog-jacket",
+    name: "Dog Jacket",
+    src: "blue-period/dog-jacket",
+  },
+  {
+    id: "steampunk-controller",
+    name: "Steampunk Controller",
+    src: "blue-period/steampunk-controller",
+  },
+  {
+    id: "bass-guitar",
+    name: "Bass Guitar",
+    src: "blue-period/bass",
+  },
+  {
+    id: "futuristic-car",
+    name: "Futuristic Car",
+    src: "blue-period/car",
+  },
+  {
+    id: "dirigible",
+    name: "Steampunk Dirigible",
+    src: "blue-period/dirigible",
+  },
+];
